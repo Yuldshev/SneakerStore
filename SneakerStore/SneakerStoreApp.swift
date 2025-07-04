f99892de12a6f8790284b4e5bcfd2fe05ff8478e
Yuldshev <ikrom921@gmail.com>
2025-07-04 16:08:44 +0500

    Initial Commit

---
+++ SneakerStore/SneakerStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SneakerStoreApp.swift
+//  SneakerStore
+//
+//  Created by T on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SneakerStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
